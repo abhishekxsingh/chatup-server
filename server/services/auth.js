4ad57253021ce064978a2422f5e1e4e2f4b43d9e
abhishekxsingh <73285773+abhishekxsingh@users.noreply.github.com>
2023-04-13 18:57:25 +0530

    completed chatup server

---
+++ server/services/auth.js
@@ -0,0 +1,43 @@
+global.users = [];
+
+const registration = async (data) => {
+
+  const isUserExist = global.users.filter(i=> i.userName===data.userName)
+  
+  if(!isUserExist.length){
+
+  global.users.push(data)
+  console.log(global.users)
+  if (data) {
+  return { doc: data };
+    }
+    return { errors: [ { name: 'User', messages: 'User already exist!' } ] };
+  }
+
+  return { errors: [ { name: 'key-name', messages: 'Something went wrong!' } ] };
+};
+
+const login = async (data) => {
+    const {userName, password} = data;
+    console.log(global.users)
+
+    const isData = global.users.find(i=>{
+      if(i.userName === userName) {
+        if(i.password === password) {
+          return i;
+        }
+        //return true
+      }
+    })
+  
+    if (isData) {
+      return { doc: {isLoggedIn: true} };
+    }
+
+    return { errors : [{key: "username", messages: "user or password is wrong"}] };
+};
+  
+module.exports = {
+  registration,
+  login
+};
